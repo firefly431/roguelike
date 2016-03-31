4189ab95f429b52bcf2ea2914ae5bde43df600ed
James Dong <firefly431.awesome@gmail.com>
2016-03-31 12:23:14 -0500

    Create types.h

---
+++ src/graphics/types.h
@@ -0,0 +1,9 @@
+#ifndef ROGUELIKE_INCLUDED_GRAPHICS_TYPES
+#define ROGUELIKE_INCLUDED_GRAPHICS_TYPES
+
+namespace graphics {
+    typedef GLTexture Texture;
+    typedef GLSprite Sprite;
+}
+
+#endif
