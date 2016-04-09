1ec450dd864969925cd9c47dcb28ca5bf02467e0
James Dong <dong.james.sept14@gmail.com>
2016-04-08 21:41:11 -0500

    Fix typos

--- src/generation/generation.cpp
+++ src/generation/generation.cpp
@@ -183,7 +183,7 @@ namespace generation {
         // hunt and kill
         while (true) {
             unsigned int cr = 0, cc = 0;
-            unsigned int er = 0; ec = 0;
+            unsigned int er = 0, ec = 0;
             // find an empty cell with a visited neighbor
             for (unsigned int r = 1; r < height - 1u; r += 2) {
                 for (unsigned int c = 1; c < width - 1u; c += 2) {
@@ -208,7 +208,7 @@ namespace generation {
     found:;
             if (er == 0) break; // we're done; maze is full
             if (cr == 0) { // we've floodfilled the section we started in
-                grid[er * width + c] = -1;
+                grid[er * width + ec] = -1;
                 continue;
             }
             while (true) {
